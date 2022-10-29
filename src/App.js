1bcd60e952b6f1c131ddf0c21071620898f9956e
orngepeel <devonbraner@gmail.com>
2022-10-29 16:03:30 -0400

    basic ui setup
    Added react-tinder-card to setup basic swiping functionality.
    getting ready for api integration.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { Search } from './pages/search';
+import { Swipe } from './pages/swipe';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Search />} />
-        {/* <Route path="/swipe" element={<Swipe />} />
+        <Route path="/swipe" element={<Swipe />} />
+        {/* 
         <Route path="/compare" element={<Compare />} /> */}
       </Routes>
     </Router>
